e1ade8a00b8946519f7f218c081690650968c9ef
j_tobiason <jtobiason@umassd.edu>
2020-04-19 21:59:18 -0400

    - Made new activity for classic game

---
+++ app/src/main/java/android/example/fliparoo/ClassicGame.java
@@ -0,0 +1,14 @@
+package android.example.fliparoo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ClassicGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_classic_game);
+    }
+}
--- app/src/main/java/android/example/fliparoo/MainActivity.java
+++ app/src/main/java/android/example/fliparoo/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //starts running main page animation
         GradientAnimation();
     }
 
